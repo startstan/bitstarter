125e63e71f7627691d3be8d9e7d991e09d73c3b8
startstan <startup.stanford@gmail.com>
2013-07-04 21:07:56 +0000

    updated web.js to get data from index.html

--- web.js
+++ web.js
@@ -2,10 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+var buffer = fs.readFileSync('./index.html');
 
+app.get('/', function(request, response) {
+  response.send(buffer.toString('ascii'));
+});
+//console.log(buffer.toString('ascii');
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
